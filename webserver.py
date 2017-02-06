d86ff6a52438e00dcc80b2e08efbefc2d9e646ab
Arunima Srivastava <arunimaasrivastava@gmail.com>
2017-02-07 03:53:49 +0530

    More efficient server.

--- webserver.py
+++ webserver.py
@@ -18,7 +18,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<li><h2>"+restaurant.name+ "</h2></li>"
                     output += "<a href='/restaurants/"+str(restaurant.id) +"/edit' style='color:blue'>Edit</a><br>" \
                                "<a href='/restaurants/"+str(restaurant.id)+"/delete' style='color:red'>Delete</a><br><br>"
-                output += "</ul></body></html>"
+                output += "</ul>"
+                output += "<br><a href='restaurants/new'>Make new Restaurant</a>"
+                output += "</body></html>"
 
                 self.wfile.write(output)
                 print output
@@ -29,7 +31,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = "<html><body>"
-                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants">' \
+                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants/new">' \
                          '<h1>Make a new restaurant</h1>' \
                          '<input name="restaurant" type="text">' \
                          '<input type="submit" value="Submit">' \
@@ -47,7 +49,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 id = re.findall(r'\d+', self.path)[0]
                 print "ID: %s"%str(id)
                 output = "<html><body>"
-                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants">' \
+                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants/edit">' \
                           '<h1>Edit Restaurant</h1>' \
                           '<input name="restaurant1" type="text">' \
                           '<input name="id" type="hidden" value="'+str(id)+'">'\
@@ -66,7 +68,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 id = re.findall(r'\d+', self.path)[0]
                 restaurant = read_restaurants.getRestaurant(int(id))
                 output = "<html><body>"
-                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants">' \
+                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants/delete">' \
                           '<h1>Are you sure you want to delete '+restaurant.name+'?</h1>' \
                           '<input name="del_id" type="hidden" value="' + str(id) + '">' \
                           '<input type="submit" value="Yes">' \
@@ -88,43 +90,41 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                restName = fields.get('restaurant')
-                name = fields.get('restaurant1')
-                id = fields.get('id')
-                del_id = fields.get('del_id')
-                print "Restaurant: %s"%restName
-                print "Name: %s"%name
-                print "Id: %s"%id
-                print "Delete ID: %s"%del_id
-                if restName!= None:
+            print self.path
+            if self.path.endswith('/new'):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    restName = fields.get('restaurant')
+                    print "Restaurant: %s" %restName
                     insert_rest.insertRestaurant(restName[0])
-                elif (name):
-                    if (id):
-                        print "Hello"
-                        read_restaurants.modifyRestaurant(int(id[0]), name[0])
-                else:
+
+            if self.path.endswith('/edit'):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    name = fields.get('restaurant1')
+                    id = fields.get('id')
+                    print "Name: %s" % name
+                    print "Id: %s" % id
+                    read_restaurants.modifyRestaurant(int(id[0]), name[0])
+
+            if self.path.endswith('/delete'):
+
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    del_id = fields.get('del_id')
+                    print "Delete ID: %s" % del_id
                     delete.deleteRestaurant(int(del_id[0]))
 
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/restaurants')
+            self.end_headers()
 
-            output = ""
-            output += "<html><body>"
-            output += "<ul>"
-            restaurants = read_restaurants.getRestaurants()
-            for restaurant in restaurants:
-                output += "<li><h2>" + restaurant.name + "</h2></li>"
-                output += "<a href='/restaurants/" + str(restaurant.id) + "/edit' style='color:blue'>Edit</a><br>" \
-                          "<a href='/restaurants/" + str(restaurant.id) + "/delete' style='color:red'>Delete</a><br><br> "
 
-            output += "</ul></body></html>"
 
-            self.wfile.write(output)
-            print output
 
         except:
             pass
