d4a79f8c8e4b43abd20804d32a7cd37077bd5635
Arunima Srivastava <arunimaasrivastava@gmail.com>
2017-02-07 02:23:23 +0530

    Add new restaurant using post method.

--- insert_rest.py
+++ insert_rest.py
@@ -7,22 +7,8 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-restaurant1 = Restaurant(name = "Picnix")
-session.add(restaurant1)
-session.commit()
-
-restaurant2 = Restaurant(name = "Cow boy")
-session.add(restaurant1)
-session.commit()
-
-restaurant3 = Restaurant(name = "Kasba")
-session.add(restaurant1)
-session.commit()
-
-restaurant4 = Restaurant(name = "bake and shake")
-session.add(restaurant1)
-session.commit()
-
-restaurant5 = Restaurant(name = "China Town")
-session.add(restaurant1)
-session.commit()
+def insertRestaurant(name):
+    """Query to insert new restaurant"""
+    restaurant = Restaurant(name= name)
+    session.add(restaurant)
+    session.commit()
--- webserver.py
+++ webserver.py
@@ -1,10 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import read_restaurants
+import read_restaurants, insert_rest
+import cgi
 
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-
             if self.path.endswith('/restaurants'):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -14,14 +14,60 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<ul>"
                 restaurants = read_restaurants.getRestaurants()
                 for restaurant in restaurants:
-                    output += "<li>"+restaurant.name+ "</li>"
+                    output += "<li><h2>"+restaurant.name+ "</h2></li>"
+                    output += "<a href='#' style='color:blue'>Edit</a><br>" \
+                               "<a href='#' style='color:red'>Delete</a><br><br>"
                 output += "</ul></body></html>"
+
+                self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith('/restaurants/new'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = "<html><body>"
+                output += '<form method="POST" enctype="multipart/form-data" action="/restaurants">' \
+                         '<h1>Make a new restaurant</h1>' \
+                         '<input name="restaurant" type="text">' \
+                         '<input type="submit" value="Submit">' \
+                         '</form>'
+                output += "</body></html>"
 
                 self.wfile.write(output)
                 print output
                 return
         except IOError:
             self.send_error(404, "File not found %s"%self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                restName = fields.get('restaurant')
+                print restName[0]
+                insert_rest.insertRestaurant(restName[0])
+
+            output = ""
+            output += "<html><body>"
+            output += "<ul>"
+            restaurants = read_restaurants.getRestaurants()
+            for restaurant in restaurants:
+                output += "<li><h2>" + restaurant.name + "</h2></li>"
+                output += "<a href='#' style='color:blue'>Edit</a><br>" \
+                          "<a href='#' style='color:red'>Delete</a><br><br>"
+            output += "</ul></body></html>"
+
+            self.wfile.write(output)
+            print output
+
+        except:
+            pass
 
 
 def main():
